262ec1110459648e293296e56e89fd94ccc662a3
Oryna Otkalenko <Arishonika@gmail.com>
2022-06-22 22:27:49 +0300

    Add project files.

---
+++ net_laba4/ComputerFacade.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace net_laba4
+{
+    public class ComputerFacade
+    {
+
+    }
+}
---
+++ net_laba4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace net_laba4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
